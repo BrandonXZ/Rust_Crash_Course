70e992004c0b68702371b643f6bf0ac5950be442
Brandon Minter <S3767526@students.fscj.edu>
2022-03-10 00:48:32 -0500

    allow dead code

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -1,9 +1,9 @@
 // This module goes a little deeper than the  first 14 modules
-
+#[allow(dead_code)]
 //Errors are split into 2 categories, recoverables and non-recoverables...will the program crash after error or is there a way to salvage and try again?
 //Rust doesn't use the idea of null, instead it used the option and result enums
 //unrecoverable examples are program stop or thread panick
-#[allow(dead_code)]
+
 pub fn run(){
     // let v = vec![1,2];                //Remove comment char to see example of a thread panick. index out of bound, there are only 2 options but we're calling the 50th option
     // v[50]                            //Backtrace=1 will walk you through how this happened(for more complex functions)
@@ -24,12 +24,14 @@ fn exit(x: i32){
                   
 
                                         //Most error types are not serious enough to close the program entirely. In most cases when we want to try again or 
+
                                         //Seek user intervention, we use the result type mentioned in the intro
+#[allow(dead_code)]
 enum Result<T,E>{
     Ok(T),
     Err(E)
 }
-
+#[allow(dead_code)]
 enum Options<T>{
     Some(T),
     None, 
