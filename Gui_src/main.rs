64318be4367f351e6ac073d657344ac0acc51c20
Brandon Minter <bsm@duostech.com>
2022-09-12 09:52:00 -0400

    adding dir for gui tutorials

---
+++ Gui_src/main.rs
@@ -0,0 +1,9 @@
+/**
+ * This is the staring point for the GUI-series instructionals. A Crash course designed to simplify all thing gui for native console apps, 
+ * those intended for web facing applications(via WASM) and those designed to be accessible from both. 
+ * Not sure if this will cover embedded sys gui's just yet as this requires a significant effort and advanced rust knowledge to implement and debug... 
+ */
+
+ pub fn main() {
+
+ }
